499e8fc1d5c5c0e9ab57a501824e95ae1e87fd0c
hbushz <hbushz@outlook.com>
2016-06-28 17:16:39 +0800

    complete the try test

--- scripts/ex4-1.sh
+++ scripts/ex4-1.sh
@@ -7,6 +7,15 @@
 #    3. file permissions
 
 echo -e "Please inpute a filename, I will check the filename's type and permission. \n\n"
-read -p "Input filename: " filename
+read -p "Input filename: "  -t 30 filename
 test -z $filename && echo "You MUST input a filename." && exit 0
 test ! -e $filename && echo "The filename $filename Do Not exist" && exit 0
+
+test -f $filename && filetype="regulare filename"
+test -d $filename && filetype="directory"
+test -r $filename && permission="readable"
+test -w $filename && permission="writable"
+test -x $filename && permission="excutable"
+
+echo "The filename: $filename is a $filetype"
+echo "And the permission are: $permission"
