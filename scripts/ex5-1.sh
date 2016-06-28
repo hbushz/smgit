33ba2fddb0f5de42be18d4897a89ea73fc4fb66c
hbushz <hbushz@outlook.com>
2016-06-28 23:22:56 +0800

    try for

--- scripts/ex5-1.sh
+++ scripts/ex5-1.sh
@@ -1,6 +1,22 @@
 #! /bin/bash
-# testing the if statement
-if date
-then
-    echo "It worked!"
-fi
+# testing the for statement
+
+# for num in 1 2 3 4 5
+# do
+#     echo "The number is $num"
+# done
+
+# for num in {1..8}
+# do
+#     echo "The number is $num"
+# done
+
+# for day in Mon Tue Wed Thu Fri Sat Sun
+# do
+#     echo "$day"
+# done
+
+for dir in $(ls /tmp)
+do
+    echo "$dir"
+done
