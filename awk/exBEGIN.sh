4e8b6c9ccd5e88b304cbf1d9013821a325477209
hbushz <hbushz@outlook.com>
2016-07-05 18:12:12 +0800

    awk learning

---
+++ awk/exBEGIN.sh
@@ -0,0 +1,8 @@
+#! /bin/awk -f
+
+BEGIN {
+x=3
+message="Hello, world!"
+print x
+print "Hello, World!"
+}
--- awk/exawk.sh
+++ awk/exawk.sh
@@ -1,6 +1,9 @@
 #! /bin/bash 
 
 # output the line begin with Character T
-result=`awk '/^T/ { print }' scores.txt`
+# result=`awk '/^T/ { print }' scores.txt`
+
+# echo "$result"
+result=`awk '/^(T|K)/ && $2>80 { print }' scores.txt`
 
 echo "$result"
