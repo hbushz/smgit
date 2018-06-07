bca3fb91d86132d88ecb059e8f4fe624d1429b05
SHUOMOPI <diphbu@163.com>
2018-06-07 22:11:19 +0800

    modify by my raspi

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
 #! /usr/bin/python3
 
-print( 'Hello world!\n')
+# age = 2
+# if age >= 18:
+#     print('Hello world, I am adult')
+# else:
+#     print('Hello world, I am young')
+a = "abc"
+b = a
+a = 123
+print(b)
