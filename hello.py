184fc1cb64ec63b795f12f698a0a1a0997844ab1
SHUOMOPI <diphbu@163.com>
2018-06-16 21:48:55 +0800

    iterable

--- hello.py
+++ hello.py
@@ -53,12 +53,81 @@
 # x1, x2 = quadratic(1, 5, 3)
 # print(x1, x2)
 
-def calc(*num):
-    sum = 1
-    for n in num:
-        sum = sum*n
-    return sum
+# def calc(*num):
+#     sum = 1
+#     for n in num:
+#         sum = sum*n
+#     return sum
 
 
-num = (1, 2, 3, 4)
-print(calc(*num))
+# num = (1, 2, 3, 4)
+# print(calc(*num))
+
+# def fact(n):
+#     if n == 1:
+#         return 1
+#     return n*fact(n-1)
+
+
+# print(fact(10))
+
+# def move(n, a, b, c):
+#     if n == 1:
+#         print(a, '-->', c)
+#     else:
+#         move(n-1, a, c, b)
+#         move(1, a, b, c)
+#         move(n-1, b, a, c)
+
+
+# move(3, "A", "B", "C")
+
+# def findMinandMax(L):
+#     if L == []:
+#         return (None, None)
+#     min = L[0]
+#     max = L[0]
+#     for t in L:
+#         if t > max:
+#             max = t
+#         elif t < min:
+#             min = t
+#     return min, max
+
+
+# print(findMinandMax([0, 1, 10, 0, 2]))
+# print(findMinandMax([]))
+
+# import os
+
+
+# d = [d for d in os.listdir('.')]
+# for myhome in d:
+#     print(myhome)
+
+# Fibonacci list generator
+# def fib(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         yield b
+#         a, b = b, a+b
+#         n = n + 1
+#     return "done"
+
+
+# for n in fib(5):
+#     print(n)
+
+# Yanghui Triangle generator
+def yht(max):
+    n = 1
+    yhl = [1]
+    while n <= max:
+        yield yhl
+        # yhl = yhl.insert(0, 0)+yhl.append(0)
+    return 'done'
+
+
+for yl in yht(3):
+    for n in yl:
+        print(n)
